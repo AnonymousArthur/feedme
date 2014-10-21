ab7622df1b89804f2ce227bd9b88815daa5ef389
Yancheng <unicrix@gmail.com>
2014-10-22 00:44:14 +1100

    location tutorial
    You can find restaurant now.

---
+++ locationTutorial.php
@@ -0,0 +1,87 @@
+<html>
+<head>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.6/css/foundation.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.6/js/vendor/modernizr.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.6/js/vendor/jquery.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.6/js/foundation.min.js"></script>
+<title>How to find restaurant</title>
+<link href='http://fonts.googleapis.com/css?family=Muli:300,400' rel='stylesheet' type='text/css'>
+</head>
+<body class="location-body">
+<nav class="top-bar" data-topbar role="navigation"> 
+  <ul class="title-area"> 
+    <li class="name"> 
+        <h1><a href="index.php">FeedMe</a></h1> 
+    </li> 
+ 
+    <li class="toggle-topbar menu-icon">
+       <a href="#"><span>Menu</span></a>
+    </li> 
+  </ul> 
+  <section class="top-bar-section"> <!-- Right Nav Section --> 
+    <ul class="right"> 
+      <li class="has-form">
+      <div class="row collapse">	
+     <div class="large-8 small-9 columns">
+      <input name="search-term" type="text" placeholder="Enter Keyword">
+       </div>
+       <div class="large-4 small-3 columns">
+         <a href="#" class=" filter alert button expand" data-filter=search-term>Search</a>
+       </div>  
+   </div>
+     </li>
+    </ul>  
+  </section> 
+</nav>
+
+<div class="location-container">
+<h1>How to find restaurant</h1>
+<div class="location-text">
+<p>The location of restaurant is very clear in this page. If the location belongs to Upper campus, so you should look at it's building code and then find a way to reach there. Here's official <a href="http://www.facilities.unsw.edu.au/sites/all/files/KENC_Campus_Sept14.pdf">Campus Map of UNSW</a>, this may helps you find your way to the restaurant.</p>
+</div>
+<div class="location-upper">
+<table class="location-container">
+<tr>
+<td>
+<img class="location-img" src="img/Upper.png"/>
+</td>
+</tr>
+<tr>
+<td>
+<label>Upper campus</label>
+</td>
+</tr>
+</table>
+</div>
+<div class="location-middle">
+<table class="location-container">
+<tr>
+<td>
+<img class="location-img" src="img/Middle.png"/>
+</td>
+</tr>
+<tr>
+<td>
+<label>Middle campus</label>
+</td>
+</tr>
+</table>
+</div>
+<div class="location-middle">
+<table class="location-container">
+<tr>
+<td>
+<img class="location-img" src="img/Lower.png"/>
+</td>
+</tr>
+<tr>
+<td>
+<label>Lower campus</label>
+</td>
+</tr>
+</table>
+</div>
+</div>
+</body>
+</html>
